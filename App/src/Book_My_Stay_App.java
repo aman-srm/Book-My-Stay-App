9196573b3c6023f9b70248671e10061f41ba2ffc
aman-srm <amanjain8574@gmail.com>
2026-03-17 15:15:58 +0530

    Application Entry & Welcome Message

---
+++ App/src/Book_My_Stay_App.java
@@ -0,0 +1,15 @@
+public class Book_My_Stay_App {
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to the Hotel Booking System!");
+
+        // Application details
+        System.out.println("Application Name: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // Inform user that application started successfully
+        System.out.println("Application initialized successfully.");
+
+    }
+}
